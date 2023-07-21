ceefd58ba5a8a4039826bee6ea82bbb478e06975
hansalbar <firdaus.hans.21@student.is.ittelkom-sby.ac.id git config --global user.name hansalbar git config --global user.email firdaus.hans.21@student.is.ittelkom-sby.ac.id>
2023-07-21 15:22:00 +0700

    codebaru1

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -1,9 +1,94 @@
 <div class="text-center">
     <img class="img-thumbnail" src="{{ Vite::asset('resources/images/gambarutama.png') }}" alt="main logo">
 </div>
-<div class=" col-md-5 order-md-2">
-    <img src="{{ Vite::asset('resources/images/LogoTajwid.png') }}" class="round" alt="...">
-</div>
+<br>
+<br>
+<div class=" text-center mx-auto mt-5" style="width: 10rem;">
+    <img src="{{ Vite::asset('resources/images/LogoTajwid.png') }}" class="card-img-top" alt="...">
+  </div>
+  <p class="text-center fs-1 fw-bold" style="color: #013033" >خيركم من يتعلم القرآن ويعلمه.</p>
+  <p class="text-center fs-2 fw-bold">"Sebaik-baik diantara kamu adalah</p>
+  <p class="text-center fs-2 fw-bold mb-5"> orang yang belajar Al-qur'an dan mengajarkannya"</p>
+
+  <div class="card-group" style="height: 25rem;">
+    <div class="card">
+      <img src="{{ Vite::asset('resources/images/kotak.png') }}" class="card-img-top" alt="...">
+      <div class="card-img-overlay">
+        <h5 class="card-title mt-5 fs-2 fw-light " style="color: #ffffff">Ingin Bisa</h5>
+        <h5 class="card-title fs-2 fw-bold " style="color: #ffffff">Mengaji Dengan</h5>
+        <h5 class="card-title fs-2  fw-bold" style="color: #ffffff">Lancar &</h5>
+        <h5 class="card-title fs-2 fw-bold " style="color: #ffffff">Sesuai Kaidah</h5>
+        <h5 class="card-title fs-2 fst-normal " style="color: #ffffff">#siapmengaji</h5>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body text-center"  style="background-color:  #c4c7c7">
+        <br>
+        <h5 class="card-title mt-5 fs-1 fw-light " style="color: #014626">Ingin Belajar</h5>
+        <h5 class="card-title fs-2 fw-bold " style="color: #014626">Ilmu tajwid Dari</h5>
+        <h5 class="card-title fs-2  fw-bold" style="color: #014626">Dasar</h5>
+        <h5 class="card-title fs-2 fst-normal " style="color: #014626">#siapmengaji</h5>
+
+      </div>
+    </div>
+    <div class="card">
+      <div class ="card-body text-center"  style="background-color:  #e4e9e9">
+        <br>
+        <h5 class="card-title mt-5 fs-2 " style="color: #014626">Ingin Belajar</h5>
+        <h5 class="card-title fs-2 fw-bold " style="color: #014626">Ilmu tajwid Dari</h5>
+        <h5 class="card-title fs-2  fw-bold" style="color: #014626">Dasar ?</h5>
+        <h5 class="card-title fs-2 " style="color: #014626">#siapmengaji</h5>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <p class="text-center fs-2 mt-5 fw-bold">Lebih Dari</p>
+ <p class="text-center fw-bold" style="font-size: 120px; color:#013033">1000 + PESERTA</P>
+    <p class="text-center fs-2 fw-bold">Telah Mengikuti Program Ini</p>
+
+    <div class="card text-bg-dark mt-5">
+    <img src="{{ Vite::asset('resources/images/panjang.png') }}" class="card-img" style="height: 30rem" alt="...">
+    <div class="card-img-overlay text-end me-5 mt-5">
+        <h5 class="card-title mt-5 fs-1  " >Ingin Belajar</h5>
+        <h5 class="card-title fs-1 fw-bold " >Ilmu Tajwid Dari</h5>
+        <a href="#" class="btn btn-light btn-lg">Daftar Sekarang</a>
+      <p class="card-text mt-2"><small>Bergabung Dengan Peserta Lainnya</small></p>
+    </div>
+  </div>
+
+  <div class="card mb-3 mt-5" style="max-width: 540px; height:15rem;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="..." class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="card mb-3 mt-5 text-end" style="max-width: 540px; height:15rem;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="..." class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 
 
 
