3494a17ba1424fc97e06239214d13d1e2b8ddd9a
Tfk19 <95390428+Tfk19@users.noreply.github.com>
2023-07-24 14:44:19 +0700

    Template Tajwidku Terbaru (#3)

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,33 @@
+<div class="footer mt-5">
+    <div class="row">
+        <div class="footer-col">
+            <div class="social-links">
+                <img class=" navbar-brand mb-0 h1" style="width: 200px; margin-top:50px"
+        src="{{ Vite::asset('resources/images/logotajwidkuputih.png') }}" alt="image" href="{{ route('home') }}">
+            </div>
+        </div>
+        <div class="footer-col">
+            <ul>
+            <h4><b>Tentang Tajwidku</b></h4>
+                <li><a href="#">Pengertian</a></li>
+                <li><a href="#">Tujuan  </a></li>
+                <li><a href="#">Hukum</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <ul>
+            <h4><b>Materi</b></h4>
+                <li><a href="#">Hukum Nun Sukun</a></li>
+                <li><a href="#">Hukum Mim Sukun</a></li>
+                <li><a href="#">Mad</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <ul>
+            <h4><b>Bimbingan</b></h4>
+                <li><a href="#">Pelajar</a></li>
+                <li><a href="#">Pengajar</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
